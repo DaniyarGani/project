2d316f09d3c6bc5f2a3e2af566b17045b81ab669
Daniyar <daniyargani866@gmail.com>
2024-02-10 16:40:20 +0600

    Functions

--- js/script.js
+++ js/script.js
@@ -1,36 +1,45 @@
-"use strict"
+"use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы смотрели?', '');
+// let num = 10;
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 20;
+//     console.log(num);
+// }
+ 
+// showFirstMessage('Hello World!');
+// console.log(num);
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из просметренных фильмов?', ''),
-          b = prompt('На сколько оцените фильм?', '');
+// function calc(a, b) {
+//     return (a + b);
+// }
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(4, 7));
+
+// function ret() {
+//     let num = 50;
+
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function() {
+//     console.log('Hello!');
+// };
+
+// logger();
+
+// const calc = a => a + b;
+// const calc = (a, b) => a + b;
+// const calc = (a, b) => { return a + b };
+
+const calc = (a, b, c) => {
+    return a + b + c;
 }
 
-if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов!');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы классичесский зритель!');
-} else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман!');
-} else {
-    console.log('Error');
-}
-
-console.log(personalMovieDB);
+console.log(calc(1, 2, 3));
