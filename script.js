49f15c0e8e2debe2632457b81059240755b2658f
Daniyar <daniyargani866@gmail.com>
2024-02-20 17:02:30 +0600

    Navigation in DOM elements

--- script.js
+++ script.js
@@ -1,43 +1,23 @@
-const btns = document.querySelectorAll('button'),
-      overlay = document.querySelector('.overlay');
-// btn.onclick = function() {
-//     alert('Click');
-// };
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastElementChild);
 
-// btn.onclick = function() {
-//     alert('Second Click');
-// };
+// console.log(document.querySelector('#current').parentNode.parentNode);
 
+// console.log(document.querySelector('[data-current="3"]').nextSibling);
+// console.log(document.querySelector('[data-current="3"]').previousSibling);
 
-// btn.addEventListener('click', () => {
-//     alert('Click');
-// });
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+// console.log(document.querySelector('[data-current="3"]').previousElementSibling);
 
-// btn.addEventListener('click', () => {
-//     alert('Second Click');
-// });
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
 
-let i = 0;
-const deleteElement = (e) => {
-    console.log(e.currentTarget);
-    console.log(e.type);
-    // i++;
-    // if (i == 1) {
-    //     btn.removeEventListener('click', deleteElement);
-    // }
-}
-
-// btn.addEventListener('click', deleteElement);
-// overlay.addEventListener('click', deleteElement);
-
-btns.forEach(btn => {
-    btn.addEventListener('click', deleteElement, {once: true});
-});
-
-const link = document.querySelector('a');
-
-link.addEventListener('click', function(event) {
-    event.preventDefault();
-
-    console.log(event.target);
-});
+    console.log(node)
+};
